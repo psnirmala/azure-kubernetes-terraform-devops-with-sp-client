852063e38257d12a4065c120b1037b965229b4d6
psnirmala <periasamy.subramanian@gmail.com>
2026-07-13 06:28:07 +0530

    Remove required_version from providers.tf
    Removed required_version constraint for Terraform.

--- providers.tf
+++ providers.tf
@@ -1,12 +1,10 @@
 terraform {
-  required_version = ">= 1.6.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
   }
-  backend "azurerm" {} # Left empty dynamically populated by Azure DevOps
 }
 
 provider "azurerm" {
