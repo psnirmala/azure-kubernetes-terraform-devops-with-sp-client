7fe1d418ec018e09da3692910e79bac9da5a3f0f
psnirmala <periasamy.subramanian@gmail.com>
2026-07-13 06:18:52 +0530

    Add Azure Container Registry and role assignment
    Added Azure Container Registry and role assignment for AKS.

--- main.tf
+++ main.tf
@@ -7,7 +7,19 @@ resource "azurerm_resource_group" "aks_rg" {
   name     = var.resource_group_name
   location = var.location
 }
+resource "azurerm_container_registry" "acr" {
+  name                = "myregistrydevops2026withsp" # Must be globally unique
+  resource_group_name = azurerm_resource_group.aks.name
+  location            = azurerm_resource_group.aks.location
+  sku                 = "Standard"
+}
 
+# Grants AKS permission to pull from your ACR
+resource "azurerm_role_assignment" "aks_to_acr" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.cluster_name
   location            = var.azurerm_resource_group.aks_rg.location
